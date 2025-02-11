125ca76d5ef0772741c653b7e474ceb7f255421e
s4nm4rkus <sanmarkusmorcoso@gmail.com>
2025-02-12 00:45:08 +0800

    Initial Commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to MyBudgetPal!</Text>
+      <Text>Welcome MyBudji!</Text>
       <StatusBar style="auto" />
     </View>
   );
